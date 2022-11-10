338e3a6b334ea37bc188bc6ddbd5bb8a65f59c31
khushboo <khushboobagoria09@gmail.com>
2022-11-10 08:12:50 +0100

    complete left and right component

--- twitter/src/components/Home/Center/CenterContent.js
+++ twitter/src/components/Home/Center/CenterContent.js
@@ -2,7 +2,7 @@
 
 function CenterContent(){
     return (
-<h2>center content here</h2>
+<h2>center content is here...</h2>
     );
 }
 export default CenterContent;
--- twitter/src/components/Home/Left/LeftContent.js
+++ twitter/src/components/Home/Left/LeftContent.js
@@ -1,6 +1,31 @@
+import './LeftContent.css';
 function LeftContent(){
     return (
-<h2>left content here</h2>
+<div class="row">
+    <div class="col-6 offset-3">
+        <div className="content-in">
+    <div className="logo">
+      <a href=""><i class="bi bi-twitter tw"></i></a>
+    </div>
+    <div className="info">
+      <a href="">
+        <div className="info-in">
+         <i class="bi bi-hash ex-icon"></i>
+         <span className="fwex-span">Explore</span>
+        </div>
+      </a>
+    </div>
+    <div className="info">
+            <a href="">
+                <div className="info-in">
+                <i class="bi bi-gear ex-icon"></i>
+                <span className="fw-span">Settings</span>
+                </div>
+            </a>
+    </div>
+</div>
+</div>
+</div>
     );
 }
 export default LeftContent;
