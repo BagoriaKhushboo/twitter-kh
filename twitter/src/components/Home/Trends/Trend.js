5a83c22fb5148a69c78ee10cba52c5593eabe29a
khushboo <khushboobagoria09@gmail.com>
2022-11-15 10:07:25 +0100

    trends component

---
+++ twitter/src/components/Home/Trends/Trend.js
@@ -0,0 +1,17 @@
+import  './Trend.css';
+function Trend(){
+    return(
+<div className="trend">
+   
+    <p className="header">
+    Trending in Netherlands
+    </p>
+    <p className="trend-text">WhatsApp</p>
+    <p className="retweet">
+    251K Tweets
+    </p>
+</div>
+
+    );
+}
+export default Trend;
