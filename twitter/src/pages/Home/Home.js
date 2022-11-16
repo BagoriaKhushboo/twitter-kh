6ea73f1ac3ef2dbd3c674dc0c1f1b7a044635e5a
khushboo <khushboobagoria09@gmail.com>
2022-11-16 12:46:39 +0100

    props

--- twitter/src/pages/Home/Home.js
+++ twitter/src/pages/Home/Home.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Home() {
+
     return (
       <div className='home'>
         <div className='leftContainer'>
