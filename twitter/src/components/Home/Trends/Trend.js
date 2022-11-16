52b72d38ec6ff6df2f4bc1f8cfe0434865e5ae01
khushboo <khushboobagoria09@gmail.com>
2022-11-16 12:47:10 +0100

    props

--- twitter/src/components/Home/Trends/Trend.js
+++ twitter/src/components/Home/Trends/Trend.js
@@ -1,15 +1,11 @@
 import  './Trend.css';
-function Trend(){
+function Trend(props){
     return(
 <div className="trend">
    
-    <p className="header">
-    Trending in Netherlands
-    </p>
-    <p className="trend-text">WhatsApp</p>
-    <p className="retweet">
-    251K Tweets
-    </p>
+    <p className="header">{props.header}</p>
+    <p className="trend-text">{props.text}</p>
+    <p className="retweet">{props.retweet}</p>
 </div>
 
     );
